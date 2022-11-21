18fc3b252267b06d4ba4865ee05fa1bcee195fd5
Bas Visscher <bas@koolecontrols.nl>
2022-11-21 16:00:14 +0100

    Default datetime format ISO

--- misc/datetime.h
+++ misc/datetime.h
@@ -5,6 +5,9 @@
 
 #include "timespan.h"
 #include "string.h"
+#include "esp_log.h"
+
+#define DEFAULTFORMAT "%Y-%m-%dT%H:%M:%S%z"
 
 namespace ESP_Base
 {
@@ -25,7 +28,7 @@ namespace ESP_Base
 		}
 		
 
-		void Set(std::string datetime, std::string format = "%FT%TZ")
+		void Set(std::string datetime, std::string format = DEFAULTFORMAT)
 		{
 			struct tm result;
 			if (strptime(datetime.c_str(), format.c_str(), &result))
@@ -55,19 +58,17 @@ namespace ESP_Base
 			return dt;
 		}	
 
-		std::string ToString() const
+		std::string ToString(std::string format = DEFAULTFORMAT) const
 		{
-			char buf[sizeof "2011-10-08T07:07:09+0100" + 1];
+			char buf[sizeof("2011-10-08T07:07:09+0100") + 1];
 			time_t t = seconds;
-			strftime(buf, sizeof buf, "%FT%T%z", localtime(&t));
-			buf[25] = '\0';
-			buf[24] = buf[23];
-			buf[23] = buf[22];
-			buf[22] = ':';
+			strftime(buf, sizeof buf, format.c_str(), localtime(&t));
+			buf[sizeof("2011-10-08T07:07:09+0100")] = '\0';
 			return buf;
 		}
-
 		
+
+	
 		friend bool operator==(DateTime const &lhs, DateTime const &rhs) { return lhs.seconds == rhs.seconds; }
 		friend bool operator!=(DateTime const &lhs, DateTime const &rhs) { return lhs.seconds != rhs.seconds; }
 		friend bool operator <(DateTime const &lhs, DateTime const &rhs) { return lhs.seconds  < rhs.seconds; }
