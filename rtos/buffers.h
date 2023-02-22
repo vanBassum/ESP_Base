cd4ddb8492fea08608a4e5d00cc6717b8f554fc9
Bas Visscher <bas@koolecontrols.nl>
2023-02-22 12:07:15 +0100

    Added event, onwritten + available bytes and free space

--- rtos/buffers.h
+++ rtos/buffers.h
@@ -14,6 +14,9 @@ namespace ESP_Base
 		
 	public:
 		
+		//Event after bytes have been written to the queue, Use to set task notifications.
+		Event<StreamBuffer*> OnWritten;
+		
 		~StreamBuffer()
 		{
 			if (handle != NULL)
@@ -22,21 +25,39 @@ namespace ESP_Base
 		
 		bool Init(size_t xBufferSizeBytes, size_t xTriggerLevelBytes)
 		{
+			if(handle != NULL)
+				vStreamBufferDelete(handle);
 			handle = xStreamBufferCreate(xBufferSizeBytes, xTriggerLevelBytes);
 			return handle != NULL;
 		}
 		
+		size_t GetAvailableBytes()
+		{
+			if (handle == NULL) return 0;
+			return xStreamBufferBytesAvailable(handle);
+		}
+		
+		size_t GetAvailableSpace()
+		{
+			if (handle == NULL) return 0;
+			return xStreamBufferSpacesAvailable(handle);
+		}
 		
 		size_t Read(void* data, size_t size) override
 		{
+			if (handle == NULL) return 0;
 			return xStreamBufferReceive(handle, data, size, portMAX_DELAY);
 		}
 		
 		size_t Write(const void* data, size_t size) override
 		{
-			return xStreamBufferSend(handle, data, size, portMAX_DELAY);
+			if (handle == NULL) return 0;
+			size_t result = xStreamBufferSend(handle, data, size, portMAX_DELAY);
+			OnWritten.Invoke(this);
+			return result;
 		}
 	};
+
 }
 
 
