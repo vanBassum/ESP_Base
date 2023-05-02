8f7261f3d7bd7cf003e2021f0341fe95cc12a4b2
Bas Visscher <bas@koolecontrols.nl>
2023-05-02 09:14:44 +0200

    Allign text in macro's

--- helpers/marcro.h
+++ helpers/marcro.h
@@ -2,22 +2,22 @@
 #include "esp_log.h"
 	    
 #define INIT_OR_RETURN(_TAG_, _MESSAGE_, FUNC)do {													\
-		bool result = (FUNC);																			\
+		bool result = (FUNC);																		\
 		if (result)																					\
-			ESP_LOGI(_TAG_, "%-64s Initialized", _MESSAGE_);										\
+			ESP_LOGI(_TAG_, "%-*s Initialized",64-strlen(_TAG_), _MESSAGE_);						\
 		else {																						\
-			ESP_LOGE(_TAG_, "%-64s Initialisation Failed", _MESSAGE_);								\
+			ESP_LOGE(_TAG_, "%-*s Initialisation Failed", 64 - strlen(_TAG_), _MESSAGE_);			\
 			return false;																			\
 		}																							\
     } while(0)	    
 	    
 	    
 #define INIT_AND_CONTINUE(_TAG_, _MESSAGE_, FUNC)do {												\
-		bool result = (FUNC);																			\
+		bool result = (FUNC);																		\
 		if (result)																					\
-			ESP_LOGI(_TAG_, "%-64s Initialized", _MESSAGE_);										\
+			ESP_LOGI(_TAG_, "%-*s Initialized",64-strlen(_TAG_), _MESSAGE_);						\
 		else {																						\
-			ESP_LOGE(_TAG_, "%-64s Initialisation Failed", _MESSAGE_);								\
+			ESP_LOGE(_TAG_, "%-*s Initialisation Failed", 64 - strlen(_TAG_), _MESSAGE_);			\
 		}																							\
     } while(0)	     
 	    
