30f4802f76291528ca2d612b054cf5f55719a95b
Bas Visscher <bas@koolecontrols.nl>
2023-08-25 09:34:46 +0200

    Added DEFINE_ENUM_CLASS_FLAG_OPERATORS

--- helpers/enum.h
+++ helpers/enum.h
@@ -10,6 +10,20 @@
 	inline T& operator&= (T& a, T b) { return (T&)((int&)a &= (int)b); } \
 	inline T& operator^= (T& a, T b) { return (T&)((int&)a ^= (int)b); } 
 
-#define HAS_BIT(a, b) 	( (a & b) > 0 )
+#define HAS_BIT(a, b) 	( (a & b) == b )
 #define HAS_ANY(a) 		( a > 0 )
+
+//https://stackoverflow.com/questions/12059774/c11-standard-conformant-bitmasks-using-enum-class
+#define DEFINE_ENUM_CLASS_FLAG_OPERATORS(T, TVAL)								 										\
+	inline constexpr T operator&(T x, T y)  { return static_cast<T> (static_cast<TVAL>(x) & static_cast<TVAL>(y));  }  	\
+	inline constexpr T  operator|(T x, T y) { return static_cast<T> (static_cast<TVAL>(x) | static_cast<TVAL>(y));  } 	\
+	inline constexpr T  operator^(T x, T y) { return static_cast<T> (static_cast<TVAL>(x) ^ static_cast<TVAL>(y));  } 	\
+	inline constexpr T  operator~(T x)		{ return static_cast<T> (~static_cast<TVAL>(x));  }                         \
+	inline T &  operator&=(T & x, T y)		{ return x = x & y; }                                                      	\
+	inline T &  operator|=(T & x, T y)		{ return x = x | y; }                                                      	\
+	inline T &  operator^=(T & x, T y)		{ return x = x ^ y; }															
+
+
+
+
 
