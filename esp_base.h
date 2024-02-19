56e8b93080f2b2f3aa8ff08cf14573ea8829a22d
Bas Visscher <bas@koolecontrols.nl>
2024-02-19 17:15:03 +0100

    Removed some unused items, todo move to drivers

--- esp_base.h
+++ esp_base.h
@@ -3,7 +3,5 @@
 #include "esp_base.h"
 
 #include "callbacks/callbacks.h"
-#include "ethernet/ethernet.h"
 #include "helpers/helpers.h"
-#include "itfs/itfs.h"
 #include "mtime/mtime.h"
--- ethernet/ethernet.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "endpoint.h"
-#include "socket.h"
-
