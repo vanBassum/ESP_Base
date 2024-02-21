a30c873f1dca783d6520053809c97884595a996e
Bas Visscher <bas@koolecontrols.nl>
2024-02-21 09:03:46 +0100

    Moved devicemanagement to base

---
+++ devicemanagement/devicemanagement.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "DeviceConfig.h"
+#include "DeviceManager.h"
+#include "DeviceTreeDetector.h"
+#include "DriverRegistry.h"
+#include "helpers.h"
+#include "IDevice.h"
+#include "IDeviceDetector.h"
+
--- esp_base.h
+++ esp_base.h
@@ -3,6 +3,7 @@
 #include "esp_base.h"
 
 #include "callbacks/callbacks.h"
+#include "devicemanagement/devicemanagement.h"
 #include "helpers/helpers.h"
 #include "mtime/mtime.h"
 #include "rtos/rtos.h"
